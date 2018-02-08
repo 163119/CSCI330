69e7a9eceb23ed509fd9d1327ded690b0a8ae0e1
163119 <36244624+163119@users.noreply.github.com>
2018-02-07 19:25:17 -0500

    Add files via upload

---
+++ src/execute.v
@@ -0,0 +1,193 @@
+module execute(pcp1, regA, regB, instr, pcpo, aluout, equalbit, muxenable, aluenable);
+
+input [31:0] pcp1;
+input [31:0] regA;
+input [31:0] regB;
+input [31:0] instr;
+input muxenable;
+input aluenable;
+
+wire [31:0] offset;
+wire [31:0] muxout;
+
+output [31:0] pcpo;
+output [31:0] aluout;
+output equalbit;
+
+signextend se(instr[15:0], offset);
+adder32 add(pcp1, offset, pcpo);
+MUX21_32B mux(regB, offset, muxenable, muxout);
+alu alu(regA, muxout, aluenable, equalbit, aluout);
+
+endmodule
+
+module IDEXReg(pcp1in, regAin, regBin, instrin, pcp1out, regAout, regBout, instrout, clk, rst);
+
+input [31:0] pcp1in;
+input [31:0] regAin;
+input [31:0] regBin;
+input [31:0] instrin;
+input clk;
+input rst;
+
+reg [31:0] pcp1;
+reg [31:0] regA;
+reg [31:0] regB;
+reg [31:0] instr;
+
+output [31:0] pcp1out;
+output [31:0] regAout;
+output [31:0] regBout;
+output [31:0] instrout;
+
+always @(posedge rst, posedge clk) begin
+	if (rst == 1) begin
+		pcp1 <= 32'd0;
+		regA <= 32'd0;
+		regB <= 32'd0;
+		instr <= 32'd0;
+	end
+end
+
+always @(posedge clk) begin
+	pcp1 <= pcp1in;
+	regA <= regAin;
+	regB <= regBin;
+	instr <= instrin;
+end
+
+assign pcp1out = pcp1;
+assign regAout = regA;
+assign regBout = regB;
+assign instrout = instr;
+
+endmodule
+
+module EXMEMReg(pcpoin, aluin, eqin, regBin, instrin, pcpoout, aluout, eqout, regBout, instrout, clk, rst);
+
+input [31:0] pcpoin;
+input [31:0] aluin;
+input [31:0] regBin;
+input [31:0] instrin;
+input eqin;
+input clk;
+input rst;
+
+reg [31:0] pcpo;
+reg [31:0] alu;
+reg [31:0] regB;
+reg [31:0] instr;
+reg equalbit;
+
+output [31:0] pcpoout;
+output [31:0] aluout;
+output [31:0] regBout;
+output [31:0] instrout;
+output eqout;
+
+always @(posedge rst, posedge clk) begin
+	if (rst == 1) begin
+		pcpo <= 32'd0;
+		alu <= 32'd0;
+		regB <= 32'd0;
+		instr <= 32'd0;
+		equalbit <= 0;
+	end
+end
+
+always @(posedge clk) begin
+	pcpo <= pcpoin;
+	alu <= aluin;
+	regB <= regBin;
+	instr <= instrin;
+	equalbit <= eqin;
+end
+
+assign pcpoout = pcpo;
+assign aluout = alu;
+assign regBout = regB;
+assign instrout = instr;
+assign eqout = equalbit;
+
+endmodule
+
+//A Full Adder
+module adder(A, B, sum, cin, cout);
+  input A, B, cin;
+  output sum, cout;
+
+  wire AB, AxorBcin, AxorB;
+
+  xor(AxorB, A, B);
+  xor(sum, AxorB, cin);//sum out
+
+  and(AxorBcin, AxorB, cin);
+  and(AB, A , B);
+  or(cout, AxorBcin, AB);//carry out
+endmodule
+
+//A 32-bit Adder
+module adder32(A, B, sum);
+  input [31:0] A, B;
+  output [31:0] sum;
+  output [31:0] cout;
+
+  genvar i;
+  adder f1(A[0], B[0], sum[0], 1'b0, cout[0]);
+  generate
+    for(i = 1; i <= 31; i = i+1)
+     begin : adders
+      adder f2(A[i], B[i], sum[i], cout[i-1], cout[i]);
+    end
+  endgenerate
+
+endmodule
+
+module signextend(in, out);
+
+input [15:0] in;
+
+output [31:0] out;
+
+//take the MSB (most significant bit) and concatenate it/replicate
+assign out = {{16{in[15]}}, in};
+
+endmodule
+
+module MUX21_32B(a, b, s, o);
+
+input wire [31:0] a;
+input wire [31:0] b;
+input wire s;
+
+output wire [31:0] o;
+
+wire ns;
+not(ns, s);
+
+wire [31:0] as;
+wire [31:0] bs;
+
+genvar bit;
+generate
+  for (bit = 0; bit < 32; bit = bit + 1) begin : select
+    and(as[bit], a[bit], ns);
+    and(bs[bit], b[bit], s);
+    or(o[bit], as[bit], bs[bit]);
+  end
+endgenerate
+
+endmodule
+
+//LC330 ALU
+module alu(A, B, op, eq, out);
+    input [31:0] A, B;
+    input op; //opcode
+    output [31:0] out; // output
+    output eq; // equality bit
+
+    assign out = (op==0)?(A + B): // add
+                 (op==1)?(A ~| B):1'bx; // nand
+    assign eq  = (A == B); // eq
+endmodule
+
---
+++ src/executeTB.v
@@ -0,0 +1,148 @@
+module executetb();
+
+	reg clk;
+	reg rst;
+
+	//IDEXReg input
+	reg [31:0] pcp1in;
+	reg [31:0] regAin;
+	reg [31:0] regBin;
+	reg [31:0] instrin;
+
+	//IDEXReg ouput wires -- DUT input
+	wire [31:0] pcp1;
+	wire [31:0] regA;
+	wire [31:0] regB;
+	wire [31:0] instr;
+
+	//DUT input
+	reg muxenable;
+	reg aluenable;
+	
+	//DUT output -- EXMEMReg input
+	wire [31:0] pcpo;
+	wire [31:0] aluout;
+	wire equalbit;
+
+	//EXMEMReg output
+	wire [31:0] instrout;
+	wire [31:0] pcpoout;
+	wire [31:0] aluout2;
+	wire [31:0] regBout;
+	wire eqout;
+
+	IDEXReg inreg(pcp1in, regAin, regBin, instrin, pcp1, regA, regB, instr, clk, rst);
+
+	execute DUT(pcp1, regA, regB, instr, pcpo, aluout, equalbit, muxenable, aluenable);
+
+	EXMEMReg outreg(pcpo, aluout, equalbit, regB, instr, pcpoout, aluout2, eqout, regBout, instrout, clk, rst);
+
+	always #1 clk = !clk;
+
+	initial begin
+		$dumpfile("test.vcd");
+		$dumpvars(0, DUT);
+		clk = 0;
+		rst = 0;
+		instrin = 32'd0;
+		regAin = 32'd0;
+		regBin = 32'd0;
+		pcp1in = 32'd0;
+		muxenable = 0;
+		aluenable = 0;
+
+		#11; //input add, no process/output
+		instrin = 32'b00000000001000100000000000000011;
+		regAin = 32'd36; //Reg $1 = 36
+		regBin = 32'd9;	//Reg $2 = 9
+		pcp1in = 32'd1;
+
+
+		#1; //input nor, process add, no output		
+		$display("IDEX UPDATED:");
+		$display("PCP1: %d", pcp1);
+		$display("REGA: %d", regA);
+		$display("REGB: %d", regB);
+		$display("INSTR: %b", instr);
+		$display("OFFSET: %d", instr[15:0]);
+		$display();
+		$display("EXMEM UPDATED:");
+		$display("PCPO: %d", pcpoout);
+		$display("EQBIT: %b", eqout);
+		$display("ALURESULT: %d", $signed(aluout2));
+		$display("REGB: %d", regBout);
+		$display("INSTR: %b", instrout);
+		$display("OFFSET: %d", instrout[15:0]);
+		$display();
+		instrin = 32'b00000000100001010000000000000110; // nand $6, $4, $5
+		regAin = 32'd18;
+		regBin = 32'd7;
+		pcp1in = 32'd2;
+
+		#2; //input lw, process nor, output add
+		$display("Add 1 2 3");
+		$display("pcpo = %d == 4?", pcpoout);
+		$display("aluresult = %d == 45?", aluout2);
+		$display("eqbit == %b == 0?", eqout);
+		$display("regB = %d == 9?", regBout);
+		$display("instr = %b == 00000000001000100000000000000011?", instrout);
+		$display("***** *****");
+		instrin = 32'b00000000010001000000000000010100;
+		regAin =32'd9;
+		regBin = 32'd18;
+		pcp1in = 32'd3;
+		aluenable = 1;
+
+		#2; //input add2, process lw, output nor
+		$display("Nor 4 5 6");
+		$display("pcpo = %d == 8?", pcpoout);
+		$display("aluresult = %d == -24 ", $signed(aluout2));
+		$display("eqbit == %b == 0?", eqout);
+		$display("regB = %d == 7?", regBout);
+		$display("instr = %b == 00000000100001010011000000000110?", instrout);
+		$display("***** *****");		
+		instrin = 32'b00000000010001010000000000000101;
+		regAin = 32'd9;
+		regBin = 32'd7;
+		pcp1in = 32'd4;
+		aluenable = 0;
+		muxenable = 1;
+
+		#2; //input sw, process add2, output lw
+		$display("lw 2 4 20");
+		$display("pcpo = %d == 23?", pcpoout);
+		$display("aluresult = %d == 29? ", aluout2);
+		$display("eqbit == %b == 0?", eqout);
+		$display("regB = %d == 18?", regBout);
+		$display("instr = %b == 00000000010001000000000000010100?", instrout);
+		$display("***** *****");
+		instrin = 32'b00000000011001110000000000001010;
+		regAin = 32'd45;
+		regBin = 32'd22;
+		pcp1in = 32'd5;
+		muxenable = 0;
+
+		#2; //no input, process sw, output add2
+		$display("add 2 5 5");
+		$display("pcpo = %d == 9?", pcpoout);
+		$display("aluresult = %d == 16? ", aluout2);
+		$display("eqbit == %b == 0?", eqout);
+		$display("regB = %d == 7?", regBout);
+		$display("instr = %b == 00000000010001010000000000000101?", instrout);
+		$display("***** *****");
+		muxenable = 1;
+
+		#2; //no input, no process, output sw
+		$display("sw 3 7 10");
+		$display("pcpo = %d == 15?", pcpoout);
+		$display("aluresult = %d == 55? ", aluout2);
+		$display("eqbit == %b == 0?", eqout);
+		$display("regB = %d == 22?", regBout);
+		$display("instr = %b == 00000000011001110000000000001010?", instrout);
+		$display("***** *****");
+		muxenable = 0;
+
+		$finish();
+	end
+
+endmodule
